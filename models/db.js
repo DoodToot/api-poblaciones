7c0129df26e56e66c739eaa3dcec1554008fdee0
afd <afd@alup5.academiapostal.es.local>
2020-07-07 16:59:46 +0200

    Obtener Poblaciones (General y por id)

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 // Primero definimos sequelize con los parámetros de conexión
-const sequelize = new Sequelize('poblaciones', 'root', ' ', {
+const sequelize = new Sequelize('poblaciones', 'root', '', {
   host: 'localhost',
   dialect: 'mariadb'
 });
